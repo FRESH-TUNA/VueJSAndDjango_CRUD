2ecb9fb1d2bc4400fab5b6d97a78b85d7fd88fbb
DONGWON KIM <lunacircle4@gmail.com>
2019-05-29 17:12:33 +0900

    repository 구현

--- API/urls.py
+++ API/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-   
+    path('', views.readPosts, name="readPosts"),
+    path('<int:pk>', views.readPost, name="readPost"),
+    path('create', views.createPost, name="createPost"),
+    path('<int:pk>/update', views.createPost, name="createPost"),
+    path('<int:pk>/delete', views.deletePost, name="deletePost"),
 ]
--- API/views.py
+++ API/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.http import JsonResponse
+from django.forms.models import model_to_dict
+import json
 
-# Create your views here.
+from .models import *
+from .forms import *
+
+
+def readPosts(request):
+    posts = list(Post.objects.values())
+    return JsonResponse({'posts': posts})
+
+def readPost(request, pk):
+    post = model_to_dict(get_object_or_404(Post, pk=pk))
+    return JsonResponse({'post': post})
+
+def createPost(request):
+    if request.method == 'POST':
+        body = json.loads(request.body.decode('utf-8'))
+        postForm = PostForm(body)
+        postForm.save()
+        return JsonResponse({'result': 'success'})
+
+def updatePost(request, pk):
+    if request.method == 'POST':
+        post = get_object_or_404(Post, pk=pk)
+        body = json.loads(request.body.decode('utf-8'))
+        postForm = PostForm(body, instance=post)
+        postForm.save()
+        return JsonResponse({'result': 'success'})
+
+def deletePost(request, pk):
+    if request.method == 'POST':
+        post = get_object_or_404(Post, pk=pk)
+        post.delete()
+        return JsonResponse({'result': 'success'})
